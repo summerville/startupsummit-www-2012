066ba80189a5241ac9b1d15811b8aa19fd47f2c8
Tony Summerville <tsummerville@rarestep.com>
2011-11-23 18:41:16 -0600

    Exported site

---
+++ exported/js/application.js
@@ -0,0 +1,10 @@
+(function() {
+  $(function() {
+    return $("a[rel='popover']").popover({
+      'placement': 'above',
+      'offset': 2
+    }).click(function(e) {
+      return e.preventDefault();
+    });
+  });
+}).call(this);
