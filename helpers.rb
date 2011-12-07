bf03f6c05fbc1bb6bc8c6c30a801c572dd24f422
Tony Summerville <tsummerville@rarestep.com>
2011-12-07 14:12:33 -0600

    Added registration success page, and tweaked sharing links

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,10 @@
 module FrankHelpers
-  # helpers go here
+  
+  def add_this_url_and_title
+    {
+      'addthis:url' => 'http://www.startupsummit.org', 
+      'addthis:title' => 'Birmingham Startup Summit: A free, one-day event featuring talks by local startup founders.'
+    }
+  end
+  
 end
